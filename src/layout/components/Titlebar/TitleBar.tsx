25387fc3ce4e268ea15da156da7168c732b7eb9a
andrwui <moralesocampoandres@gmail.com>
2024-02-27 19:28:49 -0300

    fix: corrected header styles

--- src/layout/components/Titlebar/TitleBar.tsx
+++ src/layout/components/Titlebar/TitleBar.tsx
@@ -3,18 +3,14 @@ import { type ReactElement } from 'react'
 import TFAIcon from './icons/TheFontApp.svg?react'
 import Text from 'components/Text'
 import Link from 'components/Link'
-import { LuArrowUpRight } from 'react-icons/lu'
-
 const TitleBar = (): ReactElement => {
   return (
     // The component of the top bar, currently just returns the search bar
     <div className="webkit_app-drag z-1 relative col-span-2 row-span-1 flex justify-between bg-bar-background px-6">
-      <div className="h-full flex gap-6 items-center">
-        <div className="w-[59px] h-full grid place-items-center">
-          <TFAIcon />
-        </div>
+      <div className="h-full flex gap-2 items-center">
+        <TFAIcon className="size-8" />
         <Text
-          size={24}
+          size={18}
           className="h-min"
         >
           The Font App
@@ -41,8 +37,8 @@ const TitleBar = (): ReactElement => {
           <li>
             <Link
               animated
-              to="/"
               icon
+              to="/"
             >
               Github
             </Link>
